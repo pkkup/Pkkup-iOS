d65dddcf17a42b1de7891b5b23becdf1dcb46b81
Jonathan Tsai <jontsai@jontsai.com>
2014-09-29 23:22:56 -0700

    initial commit for Pkkup-iOS

---
+++ Pkkup/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pkkup
+//
+//  Created by Jonathan Tsai on 9/29/14.
+//  Copyright (c) 2014 Pkkup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ README.md
@@ -0,0 +1,21 @@
+Pkkup-iOS
+=========
+
+This is the iOS client for [Pkkup](http://pkkup.com)
+
+## Download and Build Instructions
+
+* Clone the repository
+* Install [CocoaPods](http://cocoapods.org/)
+  * `sudo gem install cocoapods`
+* Install Pods
+  * `pod install`
+* Set up API keys
+  * Create a `secrets.xcconfig` config file based on `secrets.xcconfig.template`
+  * Obtain API keys from ... and add the keys to `secrets.xcconfig`
+* Run it!
+
+## Features
+
+* N/A
+
