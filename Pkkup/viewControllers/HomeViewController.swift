79da0917009e410d5464a6f3219541bd43ba4e1e
Jonathan Tsai <jontsai@jontsai.com>
2014-10-04 18:40:40 -0700

    added skeleton views and attempted to work on home screen

---
+++ Pkkup/viewControllers/HomeViewController.swift
@@ -0,0 +1,78 @@
+//
+//  HomeViewController.swift
+//  Pkkup
+//
+//  Created by Jonathan Tsai on 10/4/14.
+//  Copyright (c) 2014 Pkkup. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController, UISearchBarDelegate, UITableViewDataSource, UITableViewDelegate {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var sportsContainerView: UIView!
+    @IBOutlet weak var sportsContainerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var sportChoicesScrollView: UIScrollView!
+    
+    @IBOutlet weak var resultsTableView: UITableView!
+
+    let SPORT_CHOICES_DEFAULT_HEIGHT = CGFloat(44)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        hideSportChoices()
+        sportChoicesScrollView.contentSize = CGSize(width: CGFloat(1800), height: CGFloat(44))
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 15
+    }
+
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        var cell = resultsTableView.dequeueReusableCellWithIdentifier("ResultViewCell") as ResultTableViewCell
+        return cell
+    }
+
+    func hideSportChoices() {
+        sportsContainerHeightConstraint.constant = 0
+        
+    }
+
+    func showSportChoices() {
+        sportsContainerHeightConstraint.constant = SPORT_CHOICES_DEFAULT_HEIGHT
+    }
+
+    func searchBarShouldBeginEditing(searchBar: UISearchBar) -> Bool {
+        showSportChoices()
+        return true
+    }
+
+    func searchBarShouldEndEditing(searchBar: UISearchBar) -> Bool {
+        hideSportChoices()
+        return true
+    }
+
+    @IBAction func onTap(sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+        hideSportChoices()
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Pkkup/views/ResultTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  ResultTableViewCell.swift
+//  Pkkup
+//
+//  Copyright (c) 2014 Pkkup. All rights reserved.
+//
+
+import UIKit
+
+class ResultTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
