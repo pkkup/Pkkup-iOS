78c777194e8ee633e253dcb822c056d91d99e8f0
Jonathan Tsai <jontsai@jontsai.com>
2014-10-06 20:58:22 -0700

    created model stubs

---
+++ Pkkup/models/Sport.swift
@@ -0,0 +1,26 @@
+//
+//  Sport.swift
+//  Pkkup
+//
+//  Copyright (c) 2014 Pkkup. All rights reserved.
+//
+
+import Foundation
+
+class Sport {
+    var name: String?
+    // Maybe?
+    var iconImageUrl: String?
+
+    func getPlayers() -> [Player] {
+        return []
+    }
+
+    func getLocations() -> [Location] {
+        return []
+    }
+
+    func getGames() -> [Game] {
+        return []
+    }
+}
