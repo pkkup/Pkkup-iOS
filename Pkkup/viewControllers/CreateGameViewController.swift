ce75bf301a6c7876f46b1ab9ffd409e4b43ff099
grcdeepak1 <grc.deepak@gmail.com>
2014-10-19 16:02:38 -0700

    Added TabBarController

---
+++ Pkkup/viewControllers/CreateGameViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CreateGameViewController.swift
+//  Pkkup
+//
+//  Created by Deepak on 10/19/14.
+//  Copyright (c) 2014 Pkkup. All rights reserved.
+//
+
+import UIKit
+
+class CreateGameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
